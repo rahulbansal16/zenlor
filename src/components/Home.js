1236536e35254d3322df6e06a8a8ce30137ff83c
rabans <bansal.rahul14@gmail.com>
2021-10-02 15:51:34 +0530

    Added the scaffolding to fetch stylecode

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,46 @@
+import { useEffect, useState } from "react";
+import CONSTANTS from "../CONSTANTS";
+import { fetchStyleCode } from "../firebase";
+
 const PostIcon = () => {
   return (
     <img
-    //   className="zoom"
+      //   className="zoom"
       alt="Create new Style Code"
       style={{ backgroundColor: "none", borderRadius: "24px" }}
-    //   onClick={this.logEvent}
+      //   onClick={this.logEvent}
       src={require("../assets/post48.png").require}
     />
   );
 };
+
+const styleCodeCard = () => {
+  return <div></div>;
+};
+
 const Home = () => {
+  const [styleCodes, setStyleCodes] = useState(["r", "b"]);
+
+  useEffect(() => {
+    fetchStyleCode(CONSTANTS.company_id).then((responseStyleCodes) => {
+      if (responseStyleCodes) setStyleCodes(responseStyleCodes);
+    });
+  }, []);
+
+  const populateStyleCodes = () => {
+    console.log("The styleCodes are", styleCodes);
+    return (
+      <div>
+        {styleCodes.map((styleCode) => (
+          <div> {styleCode} </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div>
+      {populateStyleCodes()}
       <PostIcon />
     </div>
   );
