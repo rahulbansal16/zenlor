a52833b037ee62c15d4f01f268787b68c60e88fd
rabans <bansal.rahul14@gmail.com>
2021-10-07 15:24:59 +0530

    added prefix option

--- src/util.js
+++ src/util.js
@@ -7,13 +7,13 @@ export const getTimeStamp = () => {
 // declare all characters
 const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-export function generateUId(length) {
+export function generateUId(prefix,length) {
     let result = ' ';
     const charactersLength = characters.length;
     for ( let i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-
+    result = prefix + result.trim()
     return result.trim();
 }
 
