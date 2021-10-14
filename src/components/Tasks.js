1c054146dea9222c9754b1c1781dd1809daa6061
rabans <bansal.rahul14@gmail.com>
2021-10-14 16:37:48 +0530

    fixed the useState eror

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import ZenlorCard from "./ZenlorCard";
 import { Card, Empty, Space, Typography } from 'antd';
 import { functions } from "../firebase";
