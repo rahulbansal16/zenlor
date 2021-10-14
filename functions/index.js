6c0bb00de164b07128a4058c1f8f8fbb0d0b64a1
rabans <bansal.rahul14@gmail.com>
2021-10-14 07:55:12 +0530

    added the API for fetching tasks

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,22 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-exports.fetchTasks = functions.https.onRequest((request, respone) => {
-    functions.logger.info('In the fetchTask Functions with the request');
+admin.initializeApp();
 
+// Generate the Task on demand, Later on we will preindex the task for a particular date.
+// company/anusha_8923/style_codes/style_code_id/tasks/task_id
+// Fetch all such task initially and post that we can populate the db or store in the database
+
+// in case of ForEach there is an issue while handing the internal promises
+exports.fetchTasks = functions.https.onCall(async (data, context) => {
+        const {companyId} = data;
+        let totalTask = []
+        const styleCodesSnapshot = await admin.firestore().collection("company").doc(companyId).collection('style_codes').where('status', "==", "active").get()
+        for (let styleCodeSnapshot of styleCodesSnapshot.docs){
+            const tasksSnapshots =  await styleCodeSnapshot.ref.collection('tasks').get()
+            for (let tasksSnapshot of tasksSnapshots.docs){
+                totalTask.push(tasksSnapshot.data())
+            }
+        }
+        return totalTask
 });
