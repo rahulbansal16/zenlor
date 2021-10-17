5cca3d48d4ea1e084fbd56cb40cfb81ed70f5287
rabans <bansal.rahul14@gmail.com>
2021-10-17 18:05:22 +0530

    added code for creating task

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home"
 import Login from "./container/Login"
 import StyleCodeEditor from './components/StyleCodeEditor';
+import TaskEditor from './components/TaskEditor';
 import Header from './components/Header'
 import {auth} from './firebase';
 import { useEffect } from 'react';
@@ -16,6 +17,7 @@ const renderPages = () => {
       <Route exact path = "/" render = { () => <Tasks/>} />
       <Route exact path = "/stylecodes" render = { () => <Home/>} />
       <Route exact path = "/stylecode" render = { () => <StyleCodeEditor/>} />
+      <Route exact path = "/stylecode/:styleCodeId/task/edit" render = { (props) => <TaskEditor styleCodeId={props.match.params.styleCodeId}/>} />
       <Route exact path ="/task/:styleCodeId/:taskId" render = { (props) => <Task styleCodeId={props.match.params.styleCodeId} taskId={props.match.params.taskId}/>} />
       <Route exact path ="/tasks" render = { () => <Tasks/>}/>
       <Route exact path = "/login" render = { () => <Login/>} />
