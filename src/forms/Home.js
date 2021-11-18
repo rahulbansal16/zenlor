ad731e5945581f13364173f0fd20c80d3f25e281
rabans <bansal.rahul14@gmail.com>
2021-11-18 14:55:13 +0530

    added id as key

--- src/forms/Home.js
+++ src/forms/Home.js
@@ -30,7 +30,7 @@ const Home = ({department}) => {
             <DepartmentHeader department={department} lineNumber={lineNumber}/>
             {isFetching && <Loader/>}
             <div className="mg-y">
-                {!isFetching && updates.map( ({styleCode, styleCodeId, process, createdAt, values})  => <UpdateCard styleCode={styleCode} styleCodeId = {styleCodeId} process={process} createdAt={createdAt} data={values}/>)}
+                {!isFetching && updates.map( ({id, styleCode, styleCodeId, process, createdAt, values})  => <UpdateCard key = {id} styleCode={styleCode} styleCodeId = {styleCodeId} process={process} createdAt={createdAt} data={values}/>)}
             </div>
             {!isFetching && updates.length === 0 && <Empty/>}
             <PlusButton url = {`/${department}/process/form?lineNumber=${lineNumber||1}`}/>
