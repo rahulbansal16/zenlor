1704c44683a0d1afe3ef8b2fdd0f4148c43a08a9
rabans <bansal.rahul14@gmail.com>
2021-11-24 21:44:29 +0530

    added logic for genereare csv

--- functions/index.js
+++ functions/index.js
@@ -7,17 +7,19 @@ admin.initializeApp();
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
 const departments = [
-  "cutting",
-  "sewing",
-  "packing",
-  "washing",
-  "kajaandbuttoning"
+  {name: "cutting", lineNumber: "1"},
+  {name:"sewing", lineNumber:"1"},
+  {name:"sewing", lineNumber:"2"},
+  {name:"sewing", lineNumber:"3"},
+  {name:"packing", lineNumber:"1"},
+  {name:"washing", lineNumber:"1"},
+  {name:"kajjaandbuttoning", lineNumber:"1"},
 ]
 
 const values = {
   "cutting":["fabricIssued", "output"],
   "sewing": ["loadingReceivedQuantity", "output"],
-  "kajaandbuttoning": ["sewingReceivedQuantity", "output"],
+  "kajjaandbuttoning": ["sewingReceivedQuantity", "output"],
   "washing": ["washingReceivedQuantity", "washingSentQuantity"],
   "packing": ["washingReceivedQuantity","packedQuantity","rejectedQuantity"]
 }
@@ -46,6 +48,9 @@ exports.addData = functions
     const doc = await admin.firestore().collection("data").doc("anusha_8923").get()
     console.log("The doc is", doc.data())
     const departmentData = [entry, ...(doc.data()[department])]
+    // let obj ={
+    //   [department]: departmentData
+    // }
     let obj = {}
     obj[department] = departmentData
     await admin.firestore().collection("data").doc("anusha_8923").set(obj ,{merge: true})
@@ -57,15 +62,20 @@ exports.generateCSV = functions
 .region("asia-northeast3")
 .https.onRequest( async (request, response) => {
   let {department, lineNumber } = request.body;
-  department = department || "cutting"
+  department = department || "all"
   lineNumber = lineNumber || '1'
   lineNumber = lineNumber.toString()
 
   const doc = await admin.firestore().collection("data").doc("anusha_8923").get()
-  const departmentData = doc.data()[department]
-  console.log(departmentData)
-  const file = generateData(departmentData, department, lineNumber)
-  console.log(file)
+  const data = doc.data()
+  // console.log(departmentData)
+  let file = ""
+  if (department !== "all"){
+    file = generateData(data[department], department, lineNumber)
+  } else {
+    file += departments.map( ({name, lineNumber}) =>  generateData(data[name], name, lineNumber) )
+  }
+  // console.log(file)
   response.attachment(`${department}:${lineNumber}.csv`)
   response.type('csv')
   response.send(file)
@@ -80,19 +90,62 @@ const csvDate = (date) => {
   return moment(date, "MMM DD YY, h:mm:ss a").format("DD MMM YYYY")
 }
 
-const generateData = (departmentData ,department, line) => {
+const sum = (values1, values2) => {
+  let result = {}
+  for (let key in values1){
+    result[key] = values1[key]
+  }
+  for (let key in values2){
+    if (result[key]){
+      result[key] += values2[key]
+    } else {
+      result[key] = values2[key]
+    }
+  }
+  return result
+}
 
-  departmentData = departmentData.filter( ({lineNumber, status})  => lineNumber === line && status === "active")
+const mergeStyleCode = (data) => {
+  let hash = {}
+  for(let d of data){
+    const {createdAt, styleCode, values} = d
+    let key = styleCode + csvDate(createdAt)
+    if (!hash[key]){
+      hash[key] = {
+        values,
+        createdAt,
+        styleCode
+      }
+    } else {
+      hash[key] = {
+        styleCode,
+        values:  sum(values, hash[key].values),
+        createdAt
+      }
+    }
+  }
+  let result = []
+  console.log(hash)
+  for (let v in hash){
+    result.push(hash[v])
+  }
+  return result
+}
+
+const generateData = (departmentData ,department, line) => {
+  let filteredDepartmentData = departmentData.filter( ({lineNumber, status})  => lineNumber === line && status === "active")
+  filteredDepartmentData = mergeStyleCode(filteredDepartmentData)
   let keys = values[department]
   let fileData = ""
-  fileData += departmentData.map( row =>  {
+  fileData += filteredDepartmentData.map( row =>  {
     const {createdAt, styleCode, values} = row
     let header =  `${csvDate(createdAt)},${styleCode},`
     header += keys.map (key => values[key]||0)
     header += '\n'
     return header
   })
-  fileData = ',' + fileData
+   fileData = ',' + fileData
+   fileData = `\n${department} and lineNumber ${line}\n` + fileData
   return fileData
 }
 
