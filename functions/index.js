2215fb1e436d9ca712d854518d1d0bebf1667113
rabans <bansal.rahul14@gmail.com>
2021-11-24 14:08:31 +0530

    added data fetching api

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,27 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-// const moment = require("moment");
+const moment = require("moment");
 
 admin.initializeApp();
 
 const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+const departments = [
+  "cutting",
+  "sewing",
+  "packing",
+  "washing",
+  "kajaandbuttoning"
+]
+
+const values = {
+  "cutting":["fabricIssued", "output"],
+  "sewing": ["loadingReceivedQuantity", "output"],
+  "kajaandbuttoning": ["sewingReceivedQuantity", "output"],
+  "washing": ["washingReceivedQuantity", "washingSentQuantity"],
+  "packing": ["washingReceivedQuantity","packedQuantity","rejectedQuantity"]
+}
+
 function generateUId(prefix, length) {
   let result = " ";
   const charactersLength = characters.length;
@@ -34,6 +51,51 @@ exports.addData = functions
     await admin.firestore().collection("data").doc("anusha_8923").set(obj ,{merge: true})
     return departmentData
 })
+
+
+exports.generateCSV = functions
+.region("asia-northeast3")
+.https.onRequest( async (request, response) => {
+  let {department, lineNumber } = request.body;
+  department = department || "cutting"
+  lineNumber = lineNumber || '1'
+  lineNumber = lineNumber.toString()
+
+  const doc = await admin.firestore().collection("data").doc("anusha_8923").get()
+  const departmentData = doc.data()[department]
+  console.log(departmentData)
+  const file = generateData(departmentData, department, lineNumber)
+  console.log(file)
+  response.attachment(`${department}:${lineNumber}.csv`)
+  response.type('csv')
+  response.send(file)
+  // response.setHeader('Content-Type', 'application/vnd.openxmlformats');
+  // response.setHeader("Content-Disposition", "attachment; filename=" + department + " : " + lineNumber);
+  // response.end(file, 'binary');
+  // response.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  // response.send(file);
+})
+
+const csvDate = (date) => {
+  return moment(date, "MMM DD YY, h:mm:ss a").format("DD MMM YYYY")
+}
+
+const generateData = (departmentData ,department, line) => {
+
+  departmentData = departmentData.filter( ({lineNumber, status})  => lineNumber === line && status === "active")
+  let keys = values[department]
+  let fileData = ""
+  fileData += departmentData.map( row =>  {
+    const {createdAt, styleCode, values} = row
+    let header =  `${csvDate(createdAt)},${styleCode},`
+    header += keys.map (key => values[key]||0)
+    header += '\n'
+    return header
+  })
+  fileData = ',' + fileData
+  return fileData
+}
+
 
 
 exports.updateData = functions
