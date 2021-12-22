42c991fb294545e2af2d1bd5f4e611e6b9f60f19
rabans <bansal.rahul14@gmail.com>
2021-12-22 21:56:53 +0530

    action for fetching PO

--- src/redux/actionType.js
+++ src/redux/actionType.js
@@ -1,4 +1,5 @@
 export const FETCH_DATA = "FETCH_DATA";
 export const UPDATE_AUTH = "UPDATE_AUTH";
 export const UPDATE_STYLE_CODE_INFO = "UPDATE_STYLE_CODE_INFO";
+export const FETCH_PO = "FETCH_PO";
 export const UPDATE_ROLE = "UPDATE_ROLE";
