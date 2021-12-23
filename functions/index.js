5452436f9d7e036fda5c1876594801fe7f0bc934
rabans <bansal.rahul14@gmail.com>
2021-12-23 11:10:25 +0530

    Fixed the PO generation Logic

--- functions/index.js
+++ functions/index.js
@@ -552,7 +552,7 @@ exports.createPO = functions
 
   for (let key in supplierMap){
     purchaseOrders.push({
-      id: generateUId(10).toUpperCase(),
+      id: generateUId("",10).toUpperCase(),
       supplier: key,
       createdAt,
       status: "active",
