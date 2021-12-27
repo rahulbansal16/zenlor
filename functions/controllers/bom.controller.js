feaca493d59cdd93229a79eaae5501fefbf062db
rabans <bansal.rahul14@gmail.com>
2021-12-27 22:53:58 +0530

    fixed the issue with imports

--- functions/controllers/bom.controller.js
+++ functions/controllers/bom.controller.js
@@ -2,7 +2,7 @@ const httpStatus = require('http-status');
 const pick = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 const catchAsync = require('../utils/catchAsync');
-const bomService = require('../services');
+const {bomService} = require('../services');
 
 const createBOM = catchAsync( async (req, res) => {
     const bom = await bomService.createBOM(req.body);
