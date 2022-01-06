9cd3cee81c3a7e25de93e2aea279f4c60406a3f1
rabans <bansal.rahul14@gmail.com>
2022-01-06 21:20:32 +0530

    added Color Coded Tags

---
+++ src/components/ZenlorTags.js
@@ -0,0 +1,21 @@
+import { Tag } from "antd";
+
+const ZenlorTags = ({text}) => {
+    let color = "green"
+    switch(text.toLowerCase()){
+        case "no order":
+            color="red"
+            break;
+        case "partial order":
+            color="yellow"
+            break;
+        case "full order":
+            color="green"
+            break;
+        default:
+            color="white"
+    }
+    return <Tag color={color}>text</Tag>
+}
+
+export default ZenlorTags
