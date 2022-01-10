0af0aba1fcf9bf7c80adfb78aa6cc52d36476f81
rabans <bansal.rahul14@gmail.com>
2022-01-10 17:49:00 +0530

    lint fix function

---
+++ routes/router.ts
@@ -0,0 +1,17 @@
+// import * as express from 'express'
+// // const vendorRoute from('./vendor.route');
+// // const bomRoute from('./bom.route')
+// const router = express.Router();
+
+// const defaultRoutes = [
+//   {
+//     path: '/styleCode',
+//     route: 
+//   }
+// ];
+
+// defaultRoutes.forEach((route) => {
+//   router.use(route.path, route.route);
+// });
+
+// module.exports = router;
---
+++ routes/styleCode.route.ts
@@ -0,0 +1,17 @@
+// const express = require('express');
+// const validate = require('../../middlewares/validate');
+// const vendorValidation = require('../../validations/vendor.validation')
+// const vendorController = require('../../controllers/vendor.controller');
+// const router = express.Router();
+
+// router
+// .route("/")
+// // .get()
+// .post(validate(vendorValidation.createVendor), vendorController.createVendor)
+
+// router
+// .route("/:vendorId")
+// .get()
+
+
+// module.exports = router;
