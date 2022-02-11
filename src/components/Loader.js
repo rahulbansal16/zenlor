71b344c81bfd9bcef601d880ba337f59ce122b95
rabans <bansal.rahul14@gmail.com>
2022-02-12 00:46:37 +0530

    moved the loader to the center

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -5,6 +5,7 @@ const Loader = () => {
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
+            width:'100vw',
             height:'100vh'
         }}>
             <Space size="middle">
