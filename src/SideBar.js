59e068c81f8c373470a3761c7a47ccd44522cfde
rabans <bansal.rahul14@gmail.com>
2022-02-20 00:52:29 +0530

    added the menu

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,15 +1,19 @@
 import { Menu, Button } from 'antd';
 import {
-  AppstoreOutlined,
   MenuUnfoldOutlined,
   MenuFoldOutlined,
   PieChartOutlined,
   DesktopOutlined,
+  UpSquareOutlined,
   ContainerOutlined,
   MailOutlined,
+  ToolOutlined,
+  DollarOutlined,
+  AccountBookOutlined,
 } from '@ant-design/icons';
 import { useState } from 'react';
 import { Layout } from 'antd';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const { SubMenu } = Menu;
 const { Header, Footer, Sider, Content } = Layout;
@@ -17,10 +21,16 @@ const { Header, Footer, Sider, Content } = Layout;
 const SideBar = () => {
 
     const [collapsed, setCollapsed] = useState(false);
-    
+    const history = useHistory()
     const toggleCollapsed = () => {
         setCollapsed(!collapsed)
     };
+    const clickHandler = (e) => {
+        console.log("The click is ",e)
+        history.push({
+            pathname:`/action/${e.key}`
+        })
+    }
     return(
         <Sider collapsible collapsed={collapsed}
         style={{
@@ -31,34 +41,66 @@ const SideBar = () => {
                 {collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
             </Button>
         <Menu
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={['styleDashboard']}
           defaultOpenKeys={['sub1']}
           mode="inline"
           theme="dark"
+          onClick={(e)=>clickHandler(e)}
           inlineCollapsed={true}
         >
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            Option 1
+          <SubMenu key="style" icon={<DesktopOutlined />} title="STYLE">
+            <Menu.Item key="dashboard">Style Dashboard</Menu.Item>
+            <Menu.Item key="orderMaterials">Bill Of Materials</Menu.Item>
+          </SubMenu>
+          <SubMenu key="crm" icon={<ContainerOutlined />} title="CRM">
+          {/* Sale Orders	Costing & Pricing */}
+            <Menu.Item key="saleOrders">Sale Orders</Menu.Item>
+            <Menu.Item key="costingAndPricing">Costing And Pricing</Menu.Item>
+          </SubMenu>
+          <SubMenu key="procure" icon={<UpSquareOutlined />} title="PROCURE">
+          {/* Purchase Orders	Transfer Orders	Quotations */}
+            <Menu.Item key="purchaseOrder">Purchase Orders</Menu.Item>
+            <Menu.Item key="transferOrderQuotations">Transder Orders Quotations</Menu.Item>
+          </SubMenu>
+          {/* <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Style
           </Menu.Item>
           <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Option 2
+            CRM
           </Menu.Item>
           <Menu.Item key="3" icon={<ContainerOutlined />}>
-            Option 3
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-            <Menu.Item key="5">Option 5</Menu.Item>
+            PROCURE
+          </Menu.Item> */}
+          <SubMenu key="inventory" icon={<AccountBookOutlined />} title="INVENTORY">
+          {/* Style Dashboard	Bill of Materials */}
+          {/* Item List	Goods Received Notes	Goods Consumed Notes	Goods Dispatched Notes */}
+            <Menu.Item key="itemList">Item List</Menu.Item>
+            <Menu.Item key="inwardMaterial">Goods Received Notes</Menu.Item>
+            <Menu.Item key="goodsConsumedNotes">Goods Consumed Notes</Menu.Item>
+            <Menu.Item key="goodsDispatchedNotes">Goods Dispatched Notes</Menu.Item>
+          </SubMenu>
+          <SubMenu key="jobwork" icon={<MailOutlined />} title="JOB WORK">
+          {/* Received Orders	Issued Orders */}
+            <Menu.Item key="receivedOrders">Received Orders</Menu.Item>
+            <Menu.Item key="issuedOrders">Issued Orders</Menu.Item>
+          </SubMenu>
+          <SubMenu key="payments" icon={<DollarOutlined />} title="PAYMENTS">
+            {/* <Menu.Item key="5">Option 5</Menu.Item>
             <Menu.Item key="6">Option 6</Menu.Item>
             <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item> */}
           </SubMenu>
-          <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <SubMenu key="sub3" title="Submenu">
-              <Menu.Item key="11">Option 11</Menu.Item>
-              <Menu.Item key="12">Option 12</Menu.Item>
-            </SubMenu>
+          <SubMenu key="analytics" icon={<PieChartOutlined />} title="ANALYTICS">
+            {/* <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item> */}
+          </SubMenu>
+          <SubMenu key="tools" icon={<ToolOutlined />} title="TOOLS">
+            {/* <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item> */}
           </SubMenu>
         </Menu>
         </Sider>
